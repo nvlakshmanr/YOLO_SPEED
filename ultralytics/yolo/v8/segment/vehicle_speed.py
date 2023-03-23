abd86699105fd764f3dff64c2a3193ef003e80d4
nvlakshmanr <120115563+nvlakshmanr@users.noreply.github.com>
2023-03-23 23:06:10 +0530

    Update vehicle_speed.py

--- ultralytics/yolo/v8/segment/vehicle_speed.py
+++ ultralytics/yolo/v8/segment/vehicle_speed.py
@@ -12,8 +12,8 @@ from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 from deep_sort_pytorch.utils.parser import get_config
 from deep_sort_pytorch.deep_sort import DeepSort
 
-count_up = 0
-count_down = 0
+#count_up = 0
+#count_down = 0
 object_counter = {}
 object_counter1 = {}
 speed_line_queue ={}
@@ -102,19 +102,19 @@ def intersect(A,B,C,D):
 def ccw(A,B,C):
   return (C[1] - A[1])*(B[0]-A[0]) > (B[1]-A[1])*(C[0]-A[0])
 
-def get_direction(point1,point2):
-  direction_str = ""
-
-  if point1[1] > point2[1]:
-    direction_str += "South"
-
-  elif point1[1] < point2[1]:
-    direction_str += "North"
-
-  else:
-    direction_str += " "
-
-  return direction_str += " "
+#def get_direction(point1,point2):
+#  direction_str = ""
+#
+#  if point1[1] > point2[1]:
+#    direction_str += "South"
+#
+#  elif point1[1] < point2[1]:
+#    direction_str += "North"
+#
+#  else:
+#    direction_str += " "
+#
+#  return direction_str += " "
 
 
 
@@ -124,7 +124,6 @@ def draw_boxes(img, bbox, names,object_id, identities=None, offset=(0, 0)):
     #cv2.line(img, line[0], line[1], (46,162,112), 3)
 
     height, width, _ = img.shape
-    # remove tracked point from buffer if object is lost
     for key in list(data_deque):
       if key not in identities:
         data_deque.pop(key)
@@ -135,11 +134,7 @@ def draw_boxes(img, bbox, names,object_id, identities=None, offset=(0, 0)):
         x2 += offset[0]
         y1 += offset[1]
         y2 += offset[1]
-
-        # code to find center of bottom edge
         center = (int((x2+x1)/ 2), int((y2+y2)/2))
-
-        # get ID of object
         id = int(identities[i]) if identities is not None else 0
 
         # create new buffer for new object
@@ -153,60 +148,49 @@ def draw_boxes(img, bbox, names,object_id, identities=None, offset=(0, 0)):
         # add center to buffer
         data_deque[id].appendleft(center)
         if len(data_deque[id])>=2
-           direction = get_direction(data_deque[id][0],data_deque[id][1])
+#           direction = get_direction(data_deque[id][0],data_deque[id][1])
            object_speed = estimatespeed(data_deque[id][1],data_deque[id][0])
            speed_line_queue[id].append(object_speed)
            if intersect(data_deque[id][0],data_deque[id][1],line[0],line[1]):
              cv2.line(img,line[0],line[1],(255,255,255),3)
-             if "South" in direction :
-                if obj_name not in object_counter:
-                 object_counter[obj_name] = 1
-                 count_down += 1
-                else:
-                 object_counter[obj_name] = 1
-                 count_down += 1
-
-             if "North" in direction :
-                if obj_name not in object_counter:
-                 object_counter1[obj_name] = 1
-                 count_down += 1
-                else:
-                 object_counter1[obj_name] = 1
-                 count_down += 1
-
-
-
-
-
+#             if "South" in direction :
+#                if obj_name not in object_counter:
+#                 object_counter[obj_name] = 1
+#                 count_down += 1
+#                else:
+#                 object_counter[obj_name] = 1
+#                 count_down += 1
+#
+#             if "North" in direction :
+#                if obj_name not in object_counter:
+#                 object_counter1[obj_name] = 1
+#                 count_down += 1
+#                else:
+#                 object_counter1[obj_name] = 1
+#                 count_down += 1
         try:
           label = label +" "+str(sum(speed_line_queue[id])//len(speed_line_queue[id])) + "km/h"
         except:
           pass
 
-
-
         UI_box(box, img, label=label, color=color, line_thickness=2)
-        # draw trail
         for i in range(1, len(data_deque[id])):
-            # check if on buffer value is none
             if data_deque[id][i - 1] is None or data_deque[id][i] is None:
                 continue
-            # generate dynamic thickness of trails
             thickness = int(np.sqrt(64 / float(i + i)) * 1.5)
-            # draw trails
             cv2.line(img, data_deque[id][i - 1], data_deque[id][i], color, thickness)
         
-        # Display count in top right corner
-        for idx, (key,value)in enumerate(object_counter1.items()):
-          cnt_str = str(key + ":" +str(value)
-          cv2.putText(img,f'count of vehicles entering',(width-500,35),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
-          cv2.putText(img,"Total Count :"+str(count_up),(width-300,75),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
-          cv2.putText(img,cnt_str,(width-200,105+(idx*40)),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
-        for idx, (key,value)in enumerate(object_counter1.items()):
-          cnt_str = str(key + ":" +str(value)
-          cv2.putText(img,f'count of vehicles Leaving',(11,35),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
-          cv2.putText(img,"Total Count :"+str(count_up),(11,75),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
-          cv2.putText(img,cnt_str,(11,105+(idx*40)),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
+#        # Display count in top right corner
+#        for idx, (key,value)in enumerate(object_counter1.items()):
+#          cnt_str = str(key + ":" +str(value)
+#          cv2.putText(img,f'count of vehicles entering',(width-500,35),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
+#          cv2.putText(img,"Total Count :"+str(count_up),(width-300,75),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
+#          cv2.putText(img,cnt_str,(width-200,105+(idx*40)),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
+#        for idx, (key,value)in enumerate(object_counter1.items()):
+#          cnt_str = str(key + ":" +str(value)
+#          cv2.putText(img,f'count of vehicles Leaving',(11,35),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
+#          cv2.putText(img,"Total Count :"+str(count_up),(11,75),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
+#          cv2.putText(img,cnt_str,(11,105+(idx*40)),0,1,[255,0,0],thickness=2,lineType=CV2.LINE_AA)
             
 
     return img
